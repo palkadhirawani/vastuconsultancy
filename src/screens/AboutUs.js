5c1c87e08325cd1c830806439d6999987554d2a7
Palka Dhirawani <palkadhirawani8@gmail.com>
2021-03-22 23:13:48 +0530

    made about us page

---
+++ src/screens/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Aboutus() {
+  return (
+    <div>
+        <h1>About us page lol!</h1>
+        <p>some details about the page</p>
+    </div>
+  )
+}
+
+export default Aboutus;
