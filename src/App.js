dfe48bfff39328b37c4c3cf96099dd0a9b7cffa6
Aunalipatel <69858342+Aunalipatel@users.noreply.github.com>
2021-03-22 20:46:15 +0530

    added navbar and routes
    navbar with routes for the 5 different urls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Aboutus from './Aboutus';
+import Home from './Home';
+import Vaastuportfolio from './Vaastuportfolio';
+import Bookappointment from './Bookappointment';
+import Contact from './Contact';
+import Navbar from './Navbar';
+
+
+
 
 function App() {
   return (
+    // <div className="App">
+    //   <h1>Hello</h1>
+    // </div>
+    <Router>
     <div className="App">
-      <h1>Hello</h1>
+    <Navbar />
     </div>
+    <Switch>
+        <Route path = "/" exact component = {Home}/>;
+        <Route path = "/aboutus" exact component = {Aboutus}/>;
+        <Route path = "/contact" exact component = {Contact}/>;
+        <Route path = "/bookappointment" component = {Bookappointment}/>;
+        <Route path = "/vaastuportfolio" component = {Vaastuportfolio}/>;
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/Bookappointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bookappointment() {
+  return (
+    <div>
+        <h1>Bookapp page lol!</h1>
+    </div>
+  )
+}
+
+export default Bookappointment;
